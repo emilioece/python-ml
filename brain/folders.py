ef5273cb812bf50a7a1cd496f1b29b59cca4886b
emilioece <emailemilioee@gmail.com>
2024-04-21 13:14:15 -0700

    latest image function

--- brain/folders.py
+++ brain/folders.py
@@ -7,8 +7,8 @@ import json
 BASE_FOLDER = getcwd()
 INPUT_FOLDER = ''
 OUTPUT_FOLDER = ''
-INPUT_RELATIVE = 'static\\images'
-OUTPUT_RELATIVE = 'static\\processed_images'
+INPUT_RELATIVE = join('static', 'images')
+OUTPUT_RELATIVE = join('static', 'processed_images')
 DATABASE_FOLDER = ''
 
 
@@ -39,20 +39,18 @@ def initialize_folders():
         INPUT_PATH = join(BASE_FOLDER, INPUT_RELATIVE)
         OUTPUT_PATH = join(BASE_FOLDER, OUTPUT_RELATIVE)
         DATABASE_PATH = join(BASE_FOLDER, join('database'))
-        STATIC_FOLDER = join(BASE_FOLDER, join('static'))
         
         # Create folders
         config['INPUT_FOLDER'] = create_folder(INPUT_PATH)
         config['OUTPUT_FOLDER'] = create_folder(OUTPUT_PATH)
         config['DATABASE_FOLDER'] = create_folder(DATABASE_PATH)
-        config['STATIC_FOLDER'] = create_folder(STATIC_FOLDER)
         
         # Save configuration to JSON file
         with open('config.json', 'w') as f:
             json.dump(config, f, indent=4)
 
 def load_values():
-    global BASE_FOLDER, INPUT_FOLDER, OUTPUT_FOLDER, DATABASE_FOLDER, STATIC_FOLDER
+    global BASE_FOLDER, INPUT_FOLDER, OUTPUT_FOLDER, DATABASE_FOLDER
     initialize_folders()
     with open(CONFIG_FILE, 'r') as json_file:
         data = json.load(json_file)
@@ -63,20 +61,47 @@ def load_values():
 
 def latest_directory(directory:str) -> str:
     # List all sub-directories in directory
-    all_subdirs = [join(directory, d) for d in listdir(directory) if isdir(join(directory, d))]
+    all_subdirs = [join(directory, d) for d in listdir
+    (directory) if isdir(join(directory, d))]
+    
+    if not all_subdirs:
+        return None
     latest_subdir = max(all_subdirs, key=getmtime)
 
     # return the last modified one
-    # print(latest_subdir)
+    
     return latest_subdir
     
-def latest_image(directory: str):
-    latest_dir = latest_directory(directory)
+def latest_image(img_directory: str):
+    # check working directory
+    if img_directory == OUTPUT_FOLDER:
+        relative_path = OUTPUT_RELATIVE
+        abs_path = OUTPUT_FOLDER
+
+    elif(img_directory == INPUT_FOLDER):
     
+        relative_path = INPUT_RELATIVE
+        abs_path = INPUT_FOLDER
+    
+
+    # print(f'relative path ll {relative_path}')
+    # print(f'abs path ll {abs_path}')
+    # print(f'--- working in {img_directory} ---')
+        
+
+    latest_dir = latest_directory(img_directory)
+    # works for output since processed image stored in 
+    # processed_images/{id}/{img}
+
     if latest_dir:
         latest_image_path = max(glob(join(latest_dir, '*.jpg')), key=getmtime, default=None)
-        relative_path = join(OUTPUT_RELATIVE, relpath(latest_image_path, OUTPUT_FOLDER))
-        return relative_path
+    else:
+        latest_image_path = max(glob(join(INPUT_FOLDER,'*jpg')))
+
+    if latest_image_path:    
+        relative_img_path = relpath(latest_image_path, 'static').replace('\\', '/')
+        return relative_img_path
+
     
     return None
 
@@ -84,5 +109,7 @@ def latest_image(directory: str):
 load_values()
 
 if __name__ == '__main__':
-    image_path = latest_image(OUTPUT_FOLDER)
-    print(image_path)
+    image_paths = [INPUT_FOLDER, OUTPUT_FOLDER]
+    for image_path in image_paths:
+        patha = latest_image(image_path)
+        print(patha)
