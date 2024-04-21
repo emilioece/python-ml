206f137abe11aacdadf253134b721cb484393f31
emilioece <emailemilioee@gmail.com>
2024-04-21 13:20:19 -0700

    issue and confidence parameters in generate_response

--- brain/response.py
+++ brain/response.py
@@ -7,14 +7,14 @@ from keys import API_TOKEN
 #   issue = 'brain lesion'
 #   return (probablity, issue)
 
-def generate_response() -> str:
-    organ_status = '1'
-    issue = 'brain lesion'
+def generate_response(issue:str, confidence:str) -> str:
+    confidence = '1'
+    issue = 'lesion selar'
     organ_test = 'brain tumor xray'
-    # organ_status, issue = model_feedback() 
+    # confidence, issue = model_feedback() 
     client = OpenAI(api_key = API_TOKEN)
-    user_content = f"Organ status = {organ_status}" 
-    system_content = f"Please provide the result of your recent {organ_test}. Based on the organ_status, {organ_status} feedback, give consultation to the patient and explain what {issue} is if present, that is , if {organ_status} = 1, however, if {organ_status} = 0, explain that no problems were found and reassure good health of the patient. Make the response less than 256 tokens please."
+    user_content = f"Organ status = {confidence}" 
+    system_content = f"Please provide the result of your recent {organ_test}. Based on the confidence, {confidence} feedback, give consultation to the patient and explain what {issue} is if present, that is , if {confidence} = 1, however, if {confidence} = 0, explain that no problems were found and reassure good health of the patient. Make the response less than 256 tokens please."
 
     response = client.chat.completions.create(
       model="gpt-3.5-turbo",
@@ -34,5 +34,6 @@ def generate_response() -> str:
    
     processed_response = response.choices[0].message.content
     return processed_response
+
 if __name__ == '__main__':
-    print(generate_response())
+    print('meow')
